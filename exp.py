7dac736e8b77e8e560a33dd6859d12b60a65fda4
Mikhail Derkunov <36039811+Giocatory@users.noreply.github.com>
2023-11-18 18:37:49 +0800

    Delete exp.py

--- exp.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.datasets import mnist     # Библиотека с базой рукописных цифр
-from tensorflow.keras.models import Sequential  # Подключение класса создания модели Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Dense       # Подключение класса Dense - полносвязный слой
-from tensorflow.keras import utils              # Утилиты для подготовки данных
-import numpy as np                              # Работа с массивами
-import matplotlib.pyplot as plt                 # Отрисовка изображений
